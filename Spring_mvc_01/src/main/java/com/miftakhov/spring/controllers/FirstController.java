3d7c40b80d7e25cd8c9796ee7c4639cdb6e8f59a
Ilias <vjiowsqzojsswlrthd@kiabws.com>
2021-11-24 23:42:38 +0300

    Model plus thymeleaf for output

--- Spring_mvc_01/src/main/java/com/miftakhov/spring/controllers/FirstController.java
+++ Spring_mvc_01/src/main/java/com/miftakhov/spring/controllers/FirstController.java
@@ -1,6 +1,8 @@
 package com.miftakhov.spring.controllers;
 
+//import jdk.internal.module.IllegalAccessLogger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -44,6 +46,55 @@ public class FirstController {
         return "first/hello";
     }
 
+    @GetMapping("/hello3")
+    public String helloPage3(@RequestParam(value = "name", required = false) String name,
+                             @RequestParam(value = "surname", required = false) String surname,
+                             Model model){
+
+        model.addAttribute("message", "Hello, " + name + " " + surname);
+        // см hello.html - <p th:text="${message}" />
+        return "first/hello";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a", required = true) double a,
+                             @RequestParam(value = "b", required = true) double b,
+                             @RequestParam(value = "action", required = true) String action, Model model) {
+        double result;
+        switch (action) {
+            case ("addition"):
+                result = a + b;
+                model.addAttribute("message", "addition of " + a
+                                                    + " " + b
+                                                    + " equals "
+                                                    + result);
+                break;
+            case ("substraction"):
+                result = a - b;
+                model.addAttribute("message", "substraction of " + a
+                        + " " + b
+                        + " equals "
+                        + result);
+                break;
+            case ("multiplication"):
+                result = a * b;
+                model.addAttribute("message", "multiplication of " + a
+                        + " " + b
+                        + " equals "
+                        + result);
+                break;
+            case ("division"):
+                result = a / b;
+                model.addAttribute("message", "division of " + a
+                        + " " + b
+                        + " equals "
+                        + result);
+                break;
+            default:
+                model.addAttribute("message", "incorrect data");
+        }
+        return "first/calculator";
+    }
 
     @GetMapping("/goodbye")
     public String goodByePage(){
